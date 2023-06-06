6d80ae53bcf8a94d0037cd18d15b8f251065066d
johnpapwinter <johnpapahimonas@yahoo.com>
2023-06-06 10:38:55 +0300

    removed commented-out code

--- src/main/java/com/iknowhow/mhte/projectsexperience/configuration/WebSecurityConfig.java
+++ src/main/java/com/iknowhow/mhte/projectsexperience/configuration/WebSecurityConfig.java
@@ -1,13 +1,8 @@
 package com.iknowhow.mhte.projectsexperience.configuration;
 
-//import com.iknowhow.mhte.usersmanagement.security.AuthEntryPointJwt;
-//import com.iknowhow.mhte.usersmanagement.security.JwtFilter;
-//import com.iknowhow.mhte.usersmanagement.security.MhteAccessDeniedHandler;
 import com.iknowhow.mhte.projectsexperience.security.AuthTokenFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,21 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-//
-//    private final AuthEntryPointJwt authEntryPointJwt;
-//    private final MhteAccessDeniedHandler mhteAccessDeniedHandler;
-//    private final JwtFilter jwtFilter;
-//
-//    @Autowired
-//    WebSecurityConfig(AuthEntryPointJwt authEntryPointJwt,
-//                      MhteAccessDeniedHandler mhteAccessDeniedHandler,
-//                      JwtFilter jwtFilter) {
-//        this.authEntryPointJwt = authEntryPointJwt;
-//        this.mhteAccessDeniedHandler = mhteAccessDeniedHandler;
-//        this.jwtFilter = jwtFilter;
-//    }
-//
-    // @TODO -- IMPLEMENT USER PRINCIPAL
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -55,24 +36,4 @@ public class WebSecurityConfig {
 
         return new AuthTokenFilter();
     }
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable()
-//                .exceptionHandling()
-//                .accessDeniedHandler(mhteAccessDeniedHandler)
-//                .authenticationEntryPoint(authEntryPointJwt)
-//                .and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-//                .authorizeHttpRequests()
-//                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//                .requestMatchers("/auth/**").permitAll()
-////                .requestMatchers("/po/**").hasAnyAuthority(PoRole.PO_INITIATOR.getKey())
-////                .requestMatchers("/stats/po-status/**").authenticated()
-////                .requestMatchers("/stats/**").hasAnyAuthority(PoRole.PO_ADMIN.getKey(), PoRole.PO_FINAL_APPROVER.getKey(), PoRole.PO_APPROVER.getKey())
-//                .requestMatchers("/**")
-//                .permitAll();
-//        return http.build();
-//    }
 }
