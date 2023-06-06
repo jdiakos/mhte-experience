71777d097594c0b4afa5e8eb1d6a8ceb5f8af514
johnpapwinter <johnpapahimonas@yahoo.com>
2023-06-06 11:11:41 +0300

    set auth context holder with mhte principal

--- src/main/java/com/iknowhow/mhte/projectsexperience/security/AuthTokenFilter.java
+++ src/main/java/com/iknowhow/mhte/projectsexperience/security/AuthTokenFilter.java
@@ -1,5 +1,6 @@
 package com.iknowhow.mhte.projectsexperience.security;
 
+import com.iknowhow.mhte.authsecurity.security.MhteUserPrincipal;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,6 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -28,10 +32,14 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             if(jwt != null && jwtUtils.validateJwtToken(jwt)) {
                 String username = jwtUtils.getUsernameFromJwtToken(jwt);
 
-                // @TODO: Get MHTE principal
-                // @TODO: Create UsernamePasswordAuthenticationToken
-                // @TODO: Set authentication for SecurityContextHolder
+                // get the principal and then set the context holder with the incoming details from the parsed jwt
+                MhteUserPrincipal userPrincipal = null;
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userPrincipal, null, userPrincipal.getAuthorities()
+                );
 
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
 
         } catch (Exception e) {
