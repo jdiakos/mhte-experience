7e55bc14c5f258699e550fda55673545ed8b3eee
johnpapwinter <johnpapahimonas@yahoo.com>
2023-06-16 10:19:21 +0300

    wrapper service for project creation

---
+++ src/main/java/com/iknowhow/mhte/projectsexperience/service/DistributorWrapperServiceImpl.java
@@ -0,0 +1,26 @@
+package com.iknowhow.mhte.projectsexperience.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DistributorWrapperServiceImpl implements DistributorWrapperService {
+
+    private final ProjectService projectService;
+    private final ContractService contractService;
+    private final ProjectContractorService projectContractorService;
+    private final ProjectSubcontractorService projectSubcontractorService;
+
+    @Autowired
+    public DistributorWrapperServiceImpl(ProjectService projectService,
+                                         ContractService contractService,
+                                         ProjectContractorService projectContractorService,
+                                         ProjectSubcontractorService projectSubcontractorService) {
+        this.projectService = projectService;
+        this.contractService = contractService;
+        this.projectContractorService = projectContractorService;
+        this.projectSubcontractorService = projectSubcontractorService;
+    }
+
+
+}
