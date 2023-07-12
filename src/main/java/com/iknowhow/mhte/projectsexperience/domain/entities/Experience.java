ac5fb47ddd742c224436199e6f1d4b4816f4f50d
johnpapwinter <johnpapahimonas@yahoo.com>
2023-07-12 13:04:29 +0300

    experience entity and study categories enum

---
+++ src/main/java/com/iknowhow/mhte/projectsexperience/domain/entities/Experience.java
@@ -0,0 +1,61 @@
+package com.iknowhow.mhte.projectsexperience.domain.entities;
+
+import com.iknowhow.mhte.projectsexperience.domain.enums.StudyCategories;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "experience")
+@Getter
+@Setter
+public class Experience implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "experience_from")
+    private LocalDate experienceFrom;
+
+    @Column(name = "experience_to")
+    private LocalDate experienceTo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private StudyCategories category;
+
+    @Column(name = "value")
+    private Double value;
+
+    @Column(name = "approved_period")
+    private Long approvedPeriod;
+
+    @Column(name = "approved_value")
+    private Double approvedValue;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", referencedColumnName = "id")
+    private Project project;
+
+    @Column(name = "person_id")
+    private Long personId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Experience that = (Experience) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/iknowhow/mhte/projectsexperience/domain/enums/StudyCategories.java
@@ -0,0 +1,44 @@
+package com.iknowhow.mhte.projectsexperience.domain.enums;
+
+public enum StudyCategories {
+
+    INDUSTRIAL ("Industrial studies"),
+    TOPOGRAPHICAL ("Topographical studies"),
+    CHEMICAL ("Chemical studies"),
+    CHEMICAL_ENGINEERING_AND_INSTALLATION ("Chemical engineering and chemical installation studies"),
+    METALLURGICAL ("Metallurgical studies"),
+    GEOLOGICAL_HYDROGEOLOGICAL_GEOPHYSICAL ("Geological, hydrogeological, and geophysical studies"),
+    GEOTECHNICAL ("Geotechnical studies"),
+    EDAPHOLOGICAL ("Edaphological studies"),
+    AGRICULTURAL ("Agricultural studies"),
+    FORESTRY ("Forestry studies"),
+    GREEN_PROJECTS ("Green projects and green configuration of surrounding space"),
+    FISHERIES ("Fisheries studies"),
+    ENVIRONMENTAL ("Environmental studies"),
+    COMPUTER_NETWORK ("Computer systems and networks studies"),
+    SPATIAL_AND_REGULATORY_STUDIES ("Spatial And Regulatory Studies"),
+    URBAN_AND_SPATIAL_STUDIES ("Urban And Spatial Studies"),
+    FINANCIAL_STUDIES("Financial Studies"),
+    SOCIAL_STUDIES ("Social Studies"),
+    ORGANIZATIONAL_AND_BUSINESS_RESEARCH_STUDIES ("Organizational And Business Research Studies"),
+    ARCHITECTURAL_STUDIES_OF_BUILDING_PROJECTS ("Architectural Studies Of Building Projects"),
+    SPECIAL_ARCHITECTURAL_STUDIES ("Special Archtectural Studies"),
+    STATIC_STUDIES ("Static Studies"),
+    MECHANICAL_ELECTRICAL_ELECTRIC_STUDIES ("Mechanical, Electrical, Electric Studies"),
+    TRANSPORTATION_AND_TRAFFIC_STUDIES ("Transportation And Traffic Studies"),
+    HARBOR_WORKS_STUDIES ("Harbor Projects Studies"),
+    STUDIES_OF_MEANS_OF_TRANSPORT ("Studies Of Means Of Transport"),
+    STUDIES_OF_HYDRAULIC_PROJECTS ("Studies Of Hydraulic Projects And Water Resources Management"),
+    ENERGY_STUDIES ("Energy Studies")
+    ;
+
+    final String label;
+
+    StudyCategories(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
