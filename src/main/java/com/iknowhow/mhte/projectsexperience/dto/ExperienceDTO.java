6a8be2552d630dc0947fd3b88798fdb3ea5f8368
johnpapwinter <johnpapahimonas@yahoo.com>
2023-07-17 14:58:15 +0300

    tentative experience dto

---
+++ src/main/java/com/iknowhow/mhte/projectsexperience/dto/ExperienceDTO.java
@@ -0,0 +1,20 @@
+package com.iknowhow.mhte.projectsexperience.dto;
+
+import com.iknowhow.mhte.projectsexperience.domain.enums.ExperienceRole;
+import com.iknowhow.mhte.projectsexperience.domain.enums.StudyCategories;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class ExperienceDTO {
+
+    private Long experienceOwnerId;
+    private LocalDate experienceFrom;
+    private LocalDate experienceTo;
+    private StudyCategories category;
+    private ExperienceRole role;
+    private Double value;
+
+
+}
