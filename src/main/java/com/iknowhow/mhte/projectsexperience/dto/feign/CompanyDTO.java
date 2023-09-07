9c61fdc122f0eec73ebacfc99ae5a23aac100ce2
IKNOWHOW\mzafeiropoulos <mzafeiropoulos@iknowhow.com>
2023-09-07 17:57:48 +0300

    nothing

--- src/main/java/com/iknowhow/mhte/projectsexperience/dto/feign/CompanyDTO.java
+++ src/main/java/com/iknowhow/mhte/projectsexperience/dto/feign/CompanyDTO.java
@@ -3,6 +3,7 @@ package com.iknowhow.mhte.projectsexperience.dto.feign;
 import lombok.Data;
 import java.time.LocalDate;
 import java.util.List;
+
 import com.iknowhow.mhte.projectsexperience.domain.enums.CompanyStatusEnum;
 
 @Data
